fa38c339e593acdc2510f37573a923d0d11dc822
OneOfManyMichaels <mikaerusan@gmail.com>
2019-01-29 08:56:17 -0800

    listeners and ports open

--- server.js
+++ server.js
@@ -1,5 +1,29 @@
-//Sets up the express app
+//Dependencies
 //========================================================
+var express = require("express")
+var path = require("path")
 
+//Sets up the Express App
+//========================================================
 var app = express();
-var PORT = 3000;
+//https://nameless-ravine-94070.herokuapp.com/
+//required heroku environment variables
+var PORT = process.env.PORT || 3000;
+
+
+//Sets up the Express app to handle data parsing
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+
+
+
+
+
+
+
+//Starts the server to begin Listening
+//========================================================
+app.listen(PORT, function() {
+    console.log(`App listening on port ${port}`)
+});
