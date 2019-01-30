9781bf1e9d92ff6ae5298970286630ae9916d15d
OneOfManyMichaels <mikaerusan@gmail.com>
2019-01-29 20:13:57 -0800

    herokufail

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,90 +1,54 @@
-// The Friends storage variable that will continue to grow, 5 starter names so there will always be a match
+
+
 var friends = [
-    {
-        name: "Markus",
-        photo: "",
-        scores: [
+  {
+     "name":"Ahmed",
+     "img":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+     "answers":[
+        "5",
         "1",
-        "2",
-        "3",
+        "4",
         "4",
         "5",
-        "3",
-        "2",
-        "5",
-        "1",
-        "3"
-        ]
-    },
-  
-    {
-        name: "Allie",
-        photo: "",
-        scores: [
-        "5",
-        "5",
-        "2",
-        "3",
-        "5",
-        "2",
         "1",
         "2",
-        "4",
-        "4"
-        ]
-    },
-  
-    {
-        name: "Jones",
-        photo: "",
-        scores: [
-        "3",
-        "1",
-        "1",
-        "3",
-        "4",
         "5",
         "4",
-        "3",
-        "2",
-        "3"
-        ]
-    },
-  
-    {
-        name: "Lois",
-        photo: "",
-        scores: [
-        "1",
-        "3",
-        "5",
-        "5",
-        "2",
-        "4",
-        "1",
-        "3",
-        "2",
-        "2"
-        ]
-    },
-  
-    {
-        name: "Gerard",
-        photo: "",
-        scores: [
-        "5",
-        "3",
-        "5",
-        "2",
-        "1",
-        "2",
-        "2",
-        "3",
-        "5",
-        "4"
-        ]
-    }
-  
-  ];
-  
-  module.exports = friends;
+        "1"
+     ]
+  },
+
+  {
+    "name":"Tim",
+    "img":"",
+    "answers":[
+       "1",
+       "1",
+       "1",
+       "1",
+       "1",
+       "1",
+       "1",
+       "1",
+       "1",
+       "1"
+    ]
+ },
+ {
+  "name":"Jim",
+  "img":"",
+  "answers":[
+     "3",
+     "4",
+     "2",
+     "5",
+     "3",
+     "2",
+     "1",
+     "3",
+     "5",
+     "4"
+  ]
+},
+]
+module.exports = friends;
--- app/routing/htmlroutes.js
+++ app/routing/htmlroutes.js
@@ -1,27 +1,12 @@
-// ===============================================================================
-// DEPENDENCIES
-// Don't forget you need to require path to find paths!
-// ===============================================================================
-var path = require("path");
+var path = require('path');
 
-// ===============================================================================
-// ROUTING
-// ===============================================================================
+module.exports = function(app){
+app.get('/survey', function(req,res){
+    res.sendFile(path.join(__dirname + '/../public/survey.html'));
 
-module.exports = function(app) {
+});
+app.use(function(req, res){
+    res.sendFile(path.join(__dirname + '/../public/home.html'));
+});
 
-  // HTML GET Requests
-  // Below code handles where users go when they "visit" a page.
-  // In each of the below cases the user is shown an HTML page of content
-  // ---------------------------------------------------------------------------
-
-  //Intentional route to get to survey
-  app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/survey.html"));
-  });
-
-  // Default route when nothing else matches send them to home.html
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/home.html"));
-
-  })};
+}
